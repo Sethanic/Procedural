511883a9f316649ab6c7b52fa3b55c4afd980db8
Sethanic <cian.perill@gmail.com>
2019-05-11 11:02:36 +0100

    1st

---
+++ Assets/Scripts/MapGen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGen : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    [Range(0, 100)]
+    public int PercentFilled;
+
+    int[,] map;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        map = new int[width, height];
+    }
+}
