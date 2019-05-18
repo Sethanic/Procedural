3b7808a3799037e10d8aa0e3eaf9369b7c65844f
Unknown <cian.perill@gmail.com>
2019-05-19 00:23:27 +0100

    Mesh Renderer Added
    Ep. 3 completed

--- Assets/Scripts/MapGen.cs
+++ Assets/Scripts/MapGen.cs
@@ -17,7 +17,7 @@ public class MapGen : MonoBehaviour
 
     void Start()
     {
-        //GenerateMap();
+        GenerateMap();
     }
 
     void Update()
@@ -29,14 +29,18 @@ public class MapGen : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SmoothMap();
+            SmoothMap(4, 5);
             Debug.Log("Smoothening");
+            MeshGen meshGen = GetComponent<MeshGen>();
+            meshGen.GenerateMesh(map, 1);
         }
 
         if (Input.GetMouseButton(1))
         {
             SmoothMap();
             Debug.Log("Smoothening");
+            MeshGen meshGen = GetComponent<MeshGen>();
+            meshGen.GenerateMesh(map, 1);
         }
     }
 
@@ -45,10 +49,13 @@ public class MapGen : MonoBehaviour
         map = new int[width, height];
         RandomFillMap();
 
-        for (int i = 0; i < 5; i ++)
+        for (int i = 0; i < 16; i ++)
         {
             //SmoothMap();
         }
+
+        MeshGen meshGen = GetComponent<MeshGen>();
+        meshGen.GenerateMesh(map, 1);
     }
 
     void RandomFillMap () 
@@ -76,17 +83,22 @@ public class MapGen : MonoBehaviour
         }
     }
 
-    void SmoothMap()
+    void SmoothMap(int var1 = 4, int var2 = 4)
     {
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
                 int neighbourWallTiles = GetSurroundingWallCount(x, y);
-
-                if (neighbourWallTiles > 4)
+                // Standard     45%     4   4
+                // Archipelago  45%     4   3
+                // Simple Arch. 70%     4   5
+                // Water Splash 75%     6   4
+                // Starry Sky   95%     6   5
+                // Swiss Cheese 30%     4   3
+                if (neighbourWallTiles > var1)
                     map[x, y] = 1;
-                else if (neighbourWallTiles < 4)
+                else if (neighbourWallTiles < var2)
                     map[x, y] = 0;
 
             }
@@ -114,21 +126,5 @@ public class MapGen : MonoBehaviour
             }
         }
         return wallCount;
-    }
-
-    void OnDrawGizmos()
-    {
-        if (map != null)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
-                    Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
-                    Gizmos.DrawCube(pos, Vector3.one);
-                }
-            }
-        }
     }
 }
